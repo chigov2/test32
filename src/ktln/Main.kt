8f6d507152b7ce94880037c83cb7ba445d0f3c52
chigov2 <chigov@gmail.com>
2021-10-25 17:02:31 +0300

    functions commit

--- src/ktln/Main.kt
+++ src/ktln/Main.kt
@@ -6,4 +6,19 @@ val b: String? = "854095843"
 val c: String? = ""
     val result = (a?.length?:0) + (b?.length?:0) +(c?.length?:0)
     println(result)
+
+    val test = arrayOf(1,2,3,3,3,5,6)
+    test[4] = 9
+    println("test4 = ${test[4]}")
+
+    val array2 = arrayOfNulls<Int?>(10)
+    println(array2[3])
+
+    val listOfNumbers: MutableList<Int> = mutableListOf()
+    listOfNumbers.add(5)
+    println(listOfNumbers.get(0))
+
+    println(max(5,52))
 }
+
+fun max(a: Int, b:Int) = if (a >b) a else b
