71909de4914d653b10419f0c47dbe4d60588dfff
chigov2 <chigov@gmail.com>
2021-10-26 13:12:59 +0300

    named methods

--- src/ktln/Main.kt
+++ src/ktln/Main.kt
@@ -42,8 +42,61 @@ fun main(){
         println(array[i])
     }
     println(crop(text))
+    println(sum(5,5,5,9,6,3))
 
+    val numbers = mutableListOf<Int>(5,15,9,89,-45)
+    val result1 = sort(numbers)
+    for (i in result1)
+        println(i)
+
+    printInfo(name="Mikee")
 }
 
 fun max(a: Int, b:Int) = if (a >b) a else b
 fun crop(text:String) = text.substring(0,Math.min(7,text.length))
+
+fun sum(vararg numbers: Int): Int
+{
+    var result = 0
+    for (i in numbers)
+    {
+        result += i
+    }
+    return result
+}
+
+fun sort(numbers: MutableList<Int>): List<Int>
+{
+    for (i in 1 until numbers.size)
+    {
+        for (j in numbers.size -1 downTo i)
+        {
+            if (numbers[j] < numbers[j-1])
+            {
+                val temp = numbers[j]
+                numbers[j] = numbers[j-1]
+                numbers[j-1] = temp
+            }
+        }
+    }
+    return numbers
+}
+// перегрузка
+fun sort(numbers:Array<Int>) = sort(numbers.toMutableList())
+fun sort(vararg numbers: Int) = sort(numbers.toMutableList())
+
+fun printInfo(lastName: String = "", name: String = "", patr:String = "")
+{
+    if (lastName.isNotEmpty())
+    {
+        println(lastName)
+    }
+    if (name.isNotEmpty())
+    {
+        println(name)
+    }
+    if (patr.isNotEmpty())
+    {
+        println(patr)
+    }
+}
