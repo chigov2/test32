229140d6e2f02e035baf67db15162320fdf1f349
Max2 <chigov@gmail.com>
2021-10-26 21:35:34 +0300

    Hi_functions

--- src/ktln/HigherOrderFunctions.kt
+++ src/ktln/HigherOrderFunctions.kt
@@ -1,16 +1,25 @@
 package ktln
 
-fun main() {
+fun main1() {
     val listOfNumbers = mutableListOf<Int>()
     for (i in 0..99) {
         listOfNumbers.add(i)
         println(i)
     }
     val listOfEvenNumbers = listOfNumbers.filter ( {number:Int -> number %2 == 1} )
-    //println(listOfEvenNumbers)
-    for (i in listOfEvenNumbers)
+      for (i in listOfEvenNumbers)
     {
         println(i)
     }
 
+}
+
+fun main()
+{
+    val names = listOf("Mike","Alex","Serge")
+    val namesStartsFromA = names.filter { it.startsWith("S") }
+    for (name in namesStartsFromA)
+    {
+        println(name)
+    }
 }
