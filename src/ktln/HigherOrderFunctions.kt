dad62e9d3f445dfeef81f18ce444ee71ebd3bba3
Max2 <chigov@gmail.com>
2021-10-26 20:57:27 +0300

    listOfNumbers

--- src/ktln/HigherOrderFunctions.kt
+++ src/ktln/HigherOrderFunctions.kt
@@ -1,10 +1,16 @@
 package ktln
 
-fun main(){
-val listOfNumbers: MutableList<Int> = mutableListOf<Int>()
-for(i in 0..100)
-{
- println()
-}
+fun main() {
+    val listOfNumbers = mutableListOf<Int>()
+    for (i in 0..99) {
+        listOfNumbers.add(i)
+        println(i)
+    }
+    val listOfEvenNumbers = listOfNumbers.filter ( {number:Int -> number %2 == 1} )
+    //println(listOfEvenNumbers)
+    for (i in listOfEvenNumbers)
+    {
+        println(i)
+    }
 
 }
